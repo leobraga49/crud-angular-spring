32bea51f6c88cd460c05ec4f74f07bbb5dd502c3
leobraga49 <leonardobarrosbraga@gmail.com>
2023-06-17 10:27:50 -0300

    Create HTML form

--- frontend/src/app/courses/courses/courses.component.ts
+++ frontend/src/app/courses/courses/courses.component.ts
@@ -6,6 +6,7 @@ import { CoursesService } from './../services/courses.service';
 import { catchError, of } from 'rxjs';
 import { MatDialog } from '@angular/material/dialog';
 import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: "app-courses",
@@ -15,11 +16,13 @@ import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/err
 export class CoursesComponent {
 
   courses$: Observable<Course[]>;
-  displayedColumns = ["name", "category"];
+  displayedColumns = ["name", "category", "actions"];
 
   constructor(
     private coursesService: CoursesService,
-    public dialog: MatDialog) {
+    public dialog: MatDialog,
+    private router: Router,
+    private route: ActivatedRoute) {
     this.courses$ = this.coursesService.list()
       .pipe(
         catchError(err => {
@@ -29,10 +32,14 @@ export class CoursesComponent {
       );
   }
 
-  onError(errorMsg:string) {
+  onError(errorMsg: string) {
     this.dialog.open(ErrorDialogComponent, {
       data: errorMsg
     });
   }
+
+  onAdd() {
+    this.router.navigate(["new"], { relativeTo: this.route });
+  };
 
 }
