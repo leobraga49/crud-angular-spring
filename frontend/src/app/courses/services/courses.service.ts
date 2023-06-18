6f23f1a2088e7533b202b868f6e07a71c85b102d
leobraga49 <leonardobarrosbraga@gmail.com>
2023-06-18 10:41:03 -0300

    Put method introduced in frontend

--- frontend/src/app/courses/services/courses.service.ts
+++ frontend/src/app/courses/services/courses.service.ts
@@ -25,7 +25,19 @@ export class CoursesService {
   }
 
   save(record: Course) {
+    if (record._id) {
+      return this.update(record);
+    }
+    return this.create(record);
+  }
+
+  private create(record: Course) {
     return this.httpClient.post<Course>(this.API, record).pipe(first());
   }
+
+  private update(record: Course) {
+    return this.httpClient.put<Course>(`${this.API}/${record._id}`, record).pipe(first());
+  }
+
 
 }
