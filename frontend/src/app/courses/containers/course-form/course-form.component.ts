322ee12e277a7203be1fcc78219518ffbc155b84
leobraga49 <leonardobarrosbraga@gmail.com>
2023-06-18 10:31:21 -0300

    Populating edit form with course information

--- frontend/src/app/courses/containers/course-form/course-form.component.ts
+++ frontend/src/app/courses/containers/course-form/course-form.component.ts
@@ -2,8 +2,10 @@ import { Location } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormGroup, NonNullableFormBuilder } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute } from '@angular/router';
 
 import { CoursesService } from '../../services/courses.service';
+import { Course } from '../../model/course';
 
 @Component({
   selector: 'app-course-form',
@@ -17,10 +19,19 @@ export class CourseFormComponent {
   constructor(private formBuilder: NonNullableFormBuilder,
     private coursesService: CoursesService,
     private _snackBar: MatSnackBar,
-    private location: Location) {
+    private location: Location,
+    private route: ActivatedRoute) {
     this.form = this.formBuilder.group({
+      _id: [''],
       name: [''],
       category: [''],
+    });
+
+    const course: Course = this.route.snapshot.data["course"];
+    this.form.setValue({
+      _id: course._id,
+      name: course.name,
+      category: course.category
     });
   }
 
