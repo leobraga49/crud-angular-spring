ea656482939f82e826bb3f72b41f962b096d8f75
leobraga49 <leonardobarrosbraga@gmail.com>
2023-06-21 12:01:07 -0300

    Css refactoring in form and buttons

--- backend/crud-spring/src/main/java/com/example/controller/CourseController.java
+++ backend/crud-spring/src/main/java/com/example/controller/CourseController.java
@@ -12,6 +12,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 @Validated
 @RestController
 @AllArgsConstructor
